710072af23dcf7daea07fea463b9a432565886cf
TheMartonfi <carlmartonfi@hotmail.com>
2021-02-01 20:14:06 -0700

    Make canvas for bk start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { styled } from "styletron-react";
 
-function App() {
-	return <div>Hello World!</div>;
-}
+const Container = styled("div", () => ({
+	display: "flex",
+	justifyContent: "center",
+	backgroundColor: "black"
+}));
+
+const StartScreenCanvas = styled("div", () => ({
+	width: "65.9%",
+	height: "100vh",
+	backgroundColor: "white"
+}));
+
+const App = () => {
+	return (
+		<Container>
+			<StartScreenCanvas></StartScreenCanvas>
+		</Container>
+	);
+};
 
 export default App;
